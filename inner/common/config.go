b09b40bf9dec79b61f58536b3c596d7dbe57d98c
NikolayPraydkin <mail2193@yandex.ru>
2025-07-14 21:16:30 +0300

    Логирование

--- inner/common/config.go
+++ inner/common/config.go
@@ -6,19 +6,23 @@ import (
 )
 
 type Config struct {
-	DbDriverName string `validate:"required"`
-	Dsn          string `validate:"required"`
-	AppName      string `validate:"required"`
-	AppVersion   string `validate:"required"`
+	DbDriverName   string `validate:"required"`
+	Dsn            string `validate:"required"`
+	AppName        string `validate:"required"`
+	AppVersion     string `validate:"required"`
+	LogLevel       string
+	LogDevelopMode bool
 }
 
 func GetConfig(envFile string) Config {
 	_ = godotenv.Load(envFile)
 	var cfg = Config{
-		DbDriverName: os.Getenv("DB_DRIVER_NAME"),
-		Dsn:          os.Getenv("DB_DSN"),
-		AppName:      os.Getenv("APP_NAME"),
-		AppVersion:   os.Getenv("APP_VERSION"),
+		DbDriverName:   os.Getenv("DB_DRIVER_NAME"),
+		Dsn:            os.Getenv("DB_DSN"),
+		AppName:        os.Getenv("APP_NAME"),
+		AppVersion:     os.Getenv("APP_VERSION"),
+		LogLevel:       os.Getenv("LOG_LEVEL"),
+		LogDevelopMode: os.Getenv("LOG_DEVELOP_MODE") == "true",
 	}
 	return cfg
 }
