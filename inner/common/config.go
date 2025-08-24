9ae4990f2a2956a6cc8240decbc2ec730a0938d9
NikolayPraydkin <mail2193@yandex.ru>
2025-08-24 13:40:51 +0300

    Безопасность. SSL/TLS

--- inner/common/config.go
+++ inner/common/config.go
@@ -12,6 +12,8 @@ type Config struct {
 	AppVersion     string `validate:"required"`
 	LogLevel       string
 	LogDevelopMode bool
+	SslSert        string `validate:"required"`
+	SslKey         string `validate:"required"`
 }
 
 func GetConfig(envFile string) Config {
@@ -23,6 +25,8 @@ func GetConfig(envFile string) Config {
 		AppVersion:     os.Getenv("APP_VERSION"),
 		LogLevel:       os.Getenv("LOG_LEVEL"),
 		LogDevelopMode: os.Getenv("LOG_DEVELOP_MODE") == "true",
+		SslSert:        os.Getenv("SSL_SERT"),
+		SslKey:         os.Getenv("SSL_KEY"),
 	}
 	return cfg
 }
